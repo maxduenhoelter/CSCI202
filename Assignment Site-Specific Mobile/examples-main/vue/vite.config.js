cf28f2baf5e3be9052c61106c11fc96e7f2a6254
maxduenhoelter <maxduenhoelter@gmail.com>
2025-04-29 19:07:48 -0700

    Add files via upload

---
+++ Assignment Site-Specific Mobile/examples-main/vue/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 8080
+  },
+  css: {
+    preprocessorOptions: {
+        scss: {
+          silenceDeprecations: [
+            'import',
+            'mixed-decls',
+            'color-functions',
+            'global-builtin',
+            'legacy-js-api',
+          ],
+        },
+    },
+  },
+})
